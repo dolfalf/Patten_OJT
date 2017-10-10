5cfeb693887c06392dd5647598be163ccaaed593
jeon sangjun <tkdwnszoq@naver.com>
2017-10-10 15:25:42 +0900

    Delegate Pattern more practice

---
+++ PatternPractice/FactoryPattern.swift
@@ -0,0 +1,11 @@
+//
+//  FactoryPattern.swift
+//  PatternPractice
+//
+//  Created by jeon sangjun on 2017/10/10.
+//  Copyright © 2017年 jeon sangjun. All rights reserved.
+//
+
+import Foundation
+
+// Factoryパタンはクラスのインスタンスを作る事をサーブクラスに任せる。
